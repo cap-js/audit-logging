303ae28fc21bab8baa83cae0bcdebac4caa65501
D050513 <sebastian.van.syckel@sap.com>
2023-07-17 17:53:29 +0200

    cleanup base service

--- srv/service.js
+++ srv/service.js
@@ -5,40 +5,15 @@ const OutboxService = require('@sap/cds/libx/_runtime/messaging/Outbox')
 
 module.exports = class AuditLogService extends OutboxService {
   async init() {
-    this.before('*', function (req) {
-      req.data = _augment(req.data)
-    })
+    // add common audit log entry fields
+    this.before('*', req => req.data = _augment(req.data))
 
     // call OutboxService's init
     await super.init()
-  }
 
-  async emit(first, second) {
-    let { event, data } = typeof first === 'object' ? first : { event: first, data: second }
-    if (data.event && data.data) ({ event, data } = data)
-
-    // immediate or deferred?
-    if (!this.options.outbox) return this.send(event, data)
-    // this will open a new (detached!) tx -> preserve user
-    await this.tx(() => super.send(new cds.Request({ event, data })))
-  }
-
-  async send(event, data) {
-    if (data.event && data.data) ({ event, data } = data)
-
-    return super.send(event, data)
-  }
-
-  /*
-   * new api (await audit.log/logSync(event, data))
-   */
-
-  log(event, data = {}) {
-    return this.emit(event, data)
-  }
-
-  logSync(event, data = {}) {
-    return this.send(event, data)
+    // add self-explanatory api (await audit.log/logSync(event, data))
+    this.log = this.emit
+    this.logSync = this.send
   }
 }
 
