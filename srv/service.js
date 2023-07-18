8d2f0f16031a530292028c85409998582e17b929
D050513 <sebastian.van.syckel@sap.com>
2023-07-18 15:32:28 +0200

    augment inline

--- srv/service.js
+++ srv/service.js
@@ -6,7 +6,10 @@ const OutboxService = require('@sap/cds/libx/_runtime/messaging/Outbox')
 module.exports = class AuditLogService extends OutboxService {
   async init() {
     // add common audit log entry fields
-    this.before('*', req => req.data = _augment(req.data))
+    this.before('*', req => {
+      const { tenant, user: { id: user }, timestamp: time } = cds.context
+      Object.assign(req.data, { uuid: cds.utils.uuid(), tenant, user, time })
+    })
 
     // call OutboxService's init
     await super.init()
@@ -15,14 +18,4 @@ module.exports = class AuditLogService extends OutboxService {
     this.log = this.emit
     this.logSync = this.send
   }
-}
-
-const ANONYMOUS = 'anonymous'
-
-const _augment = data => {
-  data.uuid = cds.utils.uuid()
-  data.tenant = cds.context.tenant || ANONYMOUS
-  data.user = cds.context.user?.id || ANONYMOUS
-  data.timestamp = cds.context.timestamp
-  return data
 }
