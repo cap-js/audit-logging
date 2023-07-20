28fa5d7f99dc329a16c20ec93a992a1fa140b1fd
D050513 <sebastian.van.syckel@sap.com>
2023-07-20 15:52:10 +0200

    stay in provider account

--- test/integration/log2library-oauth2.test.js
+++ test/integration/log2library-oauth2.test.js
@@ -7,6 +7,9 @@ cds.env.requires['audit-log'] = {
   credentials: process.env.ALS_CREDS_OAUTH2 && JSON.parse(process.env.ALS_CREDS_OAUTH2)
 }
 
+// stay in provider account (i.e., use "$PROVIDER")
+cds.env.requires.auth.users.alice.tenant = cds.env.requires['audit-log'].credentials.uaa.tenantid
+
 describe('Log to Audit Log Service via library with oauth2 plan', () => {
   if (!cds.env.requires['audit-log'].credentials)
     return test.skip('Skipping tests due to missing credentials', () => {})
--- test/integration/log2restv2-oauth2.test.js
+++ test/integration/log2restv2-oauth2.test.js
@@ -7,6 +7,9 @@ cds.env.requires['audit-log'] = {
   credentials: process.env.ALS_CREDS_OAUTH2 && JSON.parse(process.env.ALS_CREDS_OAUTH2)
 }
 
+// stay in provider account (i.e., use "$PROVIDER" and avoid x-zid header when fetching oauth2 token)
+cds.env.requires.auth.users.alice.tenant = cds.env.requires['audit-log'].credentials.uaa.tenantid
+
 describe('Log to Audit Log Service via REST v2 with oauth2 plan', () => {
   if (!cds.env.requires['audit-log'].credentials)
     return test.skip('Skipping tests due to missing credentials', () => {})
