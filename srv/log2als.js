11ec4f7027cc57b10e6013b62f81e480dc7202bc
sjvans <30337871+sjvans@users.noreply.github.com>
2025-07-11 12:03:29 +0200

    ALS NG review (#164)

--- srv/log2als.js
+++ srv/log2als.js
@@ -1,13 +1,4 @@
-const cds = require('@sap/cds')
-
-const AuditLog2RESTv2 = require('./log2restv2')
-const AuditLog2RESTv3 = require('./log2restv3')
-
 const credentials = JSON.parse(process.env.VCAP_SERVICES) || {}
-const isV3 = credentials["user-provided"].some(obj => obj.tags.includes('auditlog-ng')) || {}
+const isV3 = credentials['user-provided']?.some(obj => obj.tags.includes('auditlog-ng'))
 
-if(isV3) {
-    module.exports = AuditLog2RESTv3
-} else {
-    module.exports = AuditLog2RESTv2
-}
+module.exports = isV3 ? require('./log2alsng') : require('./log2restv2')
--- test/integration/ng.test.js
+++ test/integration/ng.test.js
@@ -2,9 +2,21 @@ const cds = require('@sap/cds')
 
 const { POST } = cds.test().in(__dirname)
 
-cds.env.requires['audit-log'].kind = 'audit-log-to-restv3'
-cds.env.requires['audit-log'].impl = '@cap-js/audit-logging/srv/log2restv3'
+cds.env.requires['audit-log'].kind = 'audit-log-to-alsng'
+cds.env.requires['audit-log'].impl = '@cap-js/audit-logging/srv/log2alsng'
+const VCAP_SERVICES = {
+  'user-provided': [
+    {
+      tags: ['auditlog-ng'],
+      credentials: process.env.ALS_CREDS_NG && JSON.parse(process.env.ALS_CREDS_NG)
+    }
+  ]
+}
+process.env.VCAP_SERVICES = JSON.stringify(VCAP_SERVICES)
 
 describe('Log to Audit Log Service NG ', () => {
+  if (!VCAP_SERVICES['user-provided'][0].credentials)
+    return test.skip('Skipping tests due to missing credentials', () => {})
+
   require('./tests')(POST)
 })
