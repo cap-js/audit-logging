e4d57d6be80757ca72d031c3a4f5a737f5f4c68b
sjvans <30337871+sjvans@users.noreply.github.com>
2025-09-02 11:09:30 +0200

    fix: Correctly retrieve `appId` from the `VCAP_APPLICATION` (#178)
    Co-authored-by: simeongelovski <166386217+simeongelovski@users.noreply.github.com>

--- srv/log2alsng.js
+++ srv/log2alsng.js
@@ -12,7 +12,7 @@ module.exports = class AuditLog2ALSNG extends AuditLogService {
     this._vcap = JSON.parse(process.env.VCAP_SERVICES || '{}')
     this._userProvided = this._vcap['user-provided']?.find(obj => obj.tags.includes('auditlog-ng')) || {}
     if (!this._userProvided.credentials) throw new Error('No credentials found for SAP Audit Log Service NG')
-    this._vcapApplication = this._vcap['VCAP_APPLICATION'] || {}
+    this._vcapApplication = JSON.parse(process.env.VCAP_APPLICATION || '{}')
   }
 
   async init() {
