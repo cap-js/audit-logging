6c5c77f75097333fde5037865121a0513fe7fec2
Marten Schiwek <marten.schiwek@sap.com>
2026-02-23 13:28:53 +0100

    Update cds-plugin.js

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,69 +1,101 @@
-const cds = require('@sap/cds')
+const cds = require("@sap/cds");
 
-const { auditAccess } = require('./lib/access')
-const { addDiffToCtx, calcModLogs4Before, calcModLogs4After, emitModLogs } = require('./lib/modification')
-const { hasPersonalData } = require('./lib/utils')
+const { auditAccess } = require("./lib/access");
+const {
+  addDiffToCtx,
+  calcModLogs4Before,
+  calcModLogs4After,
+  emitModLogs,
+} = require("./lib/modification");
+const { hasPersonalData } = require("./lib/utils");
 
-const WRITE = ['CREATE', 'UPDATE', 'DELETE']
+const WRITE = ["CREATE", "UPDATE", "DELETE"];
 
 /*
  * Add generic audit logging handlers
  */
-cds.on('served', services => {
-  const db = cds.db
+cds.on("served", (services) => {
+  const db = cds.db;
 
   for (const service of services) {
-    if (!(service instanceof cds.ApplicationService)) continue
+    if (!(service instanceof cds.ApplicationService)) continue;
 
-    const relevantEntities = []
-    for (const entity of service.entities) if (hasPersonalData(entity)) relevantEntities.push(entity)
-    if (!relevantEntities.length) continue
+    const relevantEntities = [];
+    for (const entity of service.entities)
+      if (hasPersonalData(entity)) relevantEntities.push(entity);
+    if (!relevantEntities.length) continue;
 
     // automatically promote entities that are associated with data subjects
     for (const entity of relevantEntities) {
-      if (entity['@PersonalData.EntitySemantics'] !== 'DataSubject') continue
+      if (entity["@PersonalData.EntitySemantics"] !== "DataSubject") continue;
       for (const e of service.entities) {
         for (const k in e.associations) {
-          if (e.associations[k].target === entity.name && k !== 'SiblingEntity') {
-            e['@PersonalData.EntitySemantics'] ??= 'Other'
-            e.associations[k]['@PersonalData.FieldSemantics'] ??= 'DataSubjectID'
-            if (!relevantEntities.includes(e)) relevantEntities.push(e)
+          if (
+            e.associations[k].target === entity.name &&
+            k !== "SiblingEntity"
+          ) {
+            e["@PersonalData.EntitySemantics"] ??= "Other";
+            e.associations[k]["@PersonalData.FieldSemantics"] ??=
+              "DataSubjectID";
+            if (!relevantEntities.includes(e)) relevantEntities.push(e);
           }
         }
       }
     }
 
-    for (const entity of relevantEntities) {
-      /*
-       * data access
-       */
-      service.after('READ', entity, auditAccess)
+    /*
+     * data access
+     */
+    service.after("READ", async (res, req) => {
+      if (!hasPersonalData(req.target)) return;
+      await auditAccess(res, req);
+    });
 
-      /*
-       * data modification
-       */
-      // common
-      db.before(WRITE, entity, addDiffToCtx)
-      service.after(WRITE, entity, emitModLogs)
-      /*
-       * for new or modified data, modifications are calculated in after phase
-       * for deleted data, modifications are calculated in before phase
-       * deep updates can contain new, modified and deleted data -> both phases
-       */
-      // create
-      db.after('CREATE', entity, calcModLogs4After)
-      // update
-      db.before('UPDATE', entity, calcModLogs4Before)
-      db.after('UPDATE', entity, calcModLogs4After)
-      // delete
-      db.before('DELETE', entity, calcModLogs4Before)
-    }
+    /*
+     * data modification
+     */
+    service.after(WRITE, async (res, req) => {
+      if (!hasPersonalData(req.target)) return;
+      await emitModLogs(res, req);
+    });
   }
-})
+  /*
+   * data modification
+   */
+  // common
+  db.before(WRITE, async (req) => {
+    if (!hasPersonalData(req.target)) return;
+    await addDiffToCtx(req);
+  });
+  /*
+   * for new or modified data, modifications are calculated in after phase
+   * for deleted data, modifications are calculated in before phase
+   * deep updates can contain new, modified and deleted data -> both phases
+   */
+  // create
+  db.after("CREATE", async (res, req) => {
+    if (!hasPersonalData(req.target)) return;
+    await calcModLogs4After(res, req);
+  });
+  // update
+  db.before("UPDATE", async (req) => {
+    if (!hasPersonalData(req.target)) return;
+    await calcModLogs4Before(req);
+  });
+  db.after("UPDATE", async (res, req) => {
+    if (!hasPersonalData(req.target)) return;
+    await calcModLogs4After(res, req);
+  });
+  // delete
+  db.before("DELETE", async (req) => {
+    if (!hasPersonalData(req.target)) return;
+    await calcModLogs4Before(req);
+  });
+});
 
 /*
  * Export base class for extending in custom implementations
  */
 module.exports = {
-  AuditLogService: require('./srv/service')
-}
+  AuditLogService: require("./srv/service"),
+};
